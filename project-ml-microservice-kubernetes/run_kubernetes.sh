349fd3c10ae2e25c649db255ea35d845d4518f15
arshansh <sh_arsh2000@yahoo.com>
2021-03-25 00:24:21 +0000

    proj 4 nanodegree

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=arshan2000/my_machine_learning_app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment mymachinelearningapp --image=$dockerpath:latest
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/mymachinelearningapp 8080:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=arshan2000/my_machine_learning_app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag  my_machine_learning_app:latest $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
